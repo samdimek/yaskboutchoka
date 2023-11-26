83c79e835dc799b70a68a147a634a7eacb84377c
delashoo <delashoo.uk@gmail.com>
2023-11-26 09:46:12 +0300

    v2_email_sender.py

---
+++ email_sender.py
@@ -0,0 +1,27 @@
+from email.message import EmailMessage
+from app2 import password
+
+import ssl
+import smtplib
+
+email_sender = 'delashoo.uk@gmail.com'
+email_password = password
+
+email_receiver = ' '
+
+subject = "Don't forget some Oreo"
+body = """
+when you watch something try to learn.
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
